473edde7650c2ba17c27197844a31e7e410dcaf4
Harsh Kumar <harshkumar878983@gmail.com>
2024-09-18 19:10:21 +0530

    ecosystem file added

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ app.use('/', (req, res) => {
 });
 
 // SERVER
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 3001;
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });
